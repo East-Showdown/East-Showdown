9905aa8224b8f9fd1a166e0d29b154253f4b5863
KEKS1C <105987084+KEKS1C@users.noreply.github.com>
2022-12-05 20:14:25 +0300

    UKRI I ORKI RAZMNOJAUTSYA!!??!?! CHTO!?%^&*z(%#!?

--- common/defines/00_defines.lua
+++ common/defines/00_defines.lua
@@ -886,7 +886,7 @@ NMilitary = {
 	OVERSEAS_LOSE_EQUIPMENT_FACTOR = 0.75,		   -- percentage of equipment lost disbanded overseas
 	NAVAL_TRANSFER_DISBAND_MANPOWER_FACTOR = 0.5,  -- percentage of manpower returned when a naval transfering unit is disbanded
 	ENCIRCLED_DISBAND_MANPOWER_FACTOR = 0.2,       -- percentage of manpower returned when an encircled unit is disbanded
-	ORG_LOSS_FACTOR_ON_CONQUER = 0.2,              -- percentage of (max) org loss on takign enemy province
+	ORG_LOSS_FACTOR_ON_CONQUER = 0.1,              -- percentage of (max) org loss on takign enemy province
 	LOW_ORG_FOR_ATTACK = 0.3,                      -- at what org % we start affecting speed when doign hostile moves. scales down ZERO_ORG_MOVEMENT_MODIFIER
 
 	PLANNING_DECAY = 0.01,
